f5e5d2986d02635718de9dd1328d818e9ab688c2
Aashish07 <aashishpasricha7@gmail.com>
2021-01-03 17:25:55 +0530

    Explicit vs implicit

---
+++ implicit_vs_Explicit.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var number = 2000  -> implicit defined
+	//var num uint16 = 1000  -> explicitly defined
+
+	//expression assignment operator
+	//omitting var keyword
+	//number := 6
+	number := 6.01
+	var bl bool
+	fmt.Println(number, bl)
+}
