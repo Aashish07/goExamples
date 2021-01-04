c1192e318f2245801e6b89fa45ba14493c1de1fc
Aashish07 <aashishpasricha7@gmail.com>
2021-01-04 15:48:02 +0530

    Adding console formatter

---
+++ Previewfmt.md
@@ -0,0 +1,15 @@
+General
+    1. %v (value in default format)
+    2. %T (type)
+    3. %% (literal %)
+
+Boolean
+    1. %t (true or false)
+
+Integer
+
+    1. %b (base 2) binary
+    2. %o (base 8) octal
+    3. %d (base 10) decimal
+    4. %x (base 16) hexadecimal small %x caps %X
+
---
+++ consoleFmtPrint.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// %T -> will print value
+	// %v will print values
+	//printf will print out the format
+	//sprintf will let us store that value in the variable
+	//Sprintf formats according to a format specifier and returns the resulting string.
+	fmt.Printf("Hello %X", 3435)
+	fmt.Println("")
+}
